ac8aae319bd4ff0422aeefac1c8129bfb507f6e3
Jacob Van Vleet <jacob.vanvleet@gmail.com>
2015-09-03 15:47:24 -0400

    test

--- models/stores.js
+++ models/stores.js
@@ -5,13 +5,13 @@ module.exports = function(sequelize, DataTypes) {
     storeName: { type: DataTypes.STRING, allowNull: false },
 		type: { type: DataTypes.STRING, allowNull: false },
 		accountName: { type: DataTypes.STRING, allowNull: false },
-	}/*, {
+	}, {
 		classMethods: {
 			associate: function(models){
 				STORES.hasOne(models.INFO, {as: 'storeID'});
         STORES.hasOne(models.TOKENS, {as: 'storeID'});
 			}
 		}
-	}*/);
+	});
 	return STORES;
 };
