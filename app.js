14c6281cea31ee3b0365d7a57cb3255dc55f4b26
Jacob Van Vleet <jacob.vanvleet@gmail.com>
2015-09-03 11:44:24 -0400

    test

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 
@@ -19,8 +16,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+require('./routes/router.js')(app);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -36,7 +32,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.send(500);
+    res.sendStatus(500);
   });
 }
 
@@ -44,7 +40,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.send(500);
+  res.sendStatus(500);
 });
 
 
