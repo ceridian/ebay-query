0bc20c14dd33709720aea90fb6a3bb7531c2b8db
Jacob Van Vleet <jacob.vanvleet@gmail.com>
2015-09-03 12:16:17 -0400

    test

--- app.js
+++ app.js
@@ -18,30 +18,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 require('./routes/router.js')(app);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.get('/404', function(req, res, next){
+  next();
+});
+
+// 404
+
+app.use(function(req, res, next){
+  res.redirect('/');
 });
 
 // error handlers
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.sendStatus(500);
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.sendStatus(500);
+  if(err){
+  	res.status(err.status || 500);
+  	res.send(err);
+  }else{
+    res.redirect('/');
+  }
 });
-
 
 module.exports = app;
