81beffd2470d81196a9cb587ba47e08126c1c714
Jacob Van Vleet <jacob.vanvleet@gmail.com>
2015-09-04 10:02:44 -0400

    test

--- routes/router.js
+++ routes/router.js
@@ -3,7 +3,7 @@ var lib = require('../lib/lib.js');
 
 module.exports = function(app) {
   app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname, '../public', 'home.html'));
+    res.sendFile(path.join(__dirname, '../public', 'index.html'));
   });
 
   app.post('/messages', function(req, res){
