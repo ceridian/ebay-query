cfa93bcbccdd7b4a9fa41b173f9b9d3ba5cc691a
Jacob Van Vleet <jacob.vanvleet@gmail.com>
2015-09-04 14:07:34 -0400

    test

--- public/js/app.js
+++ public/js/app.js
@@ -1,31 +1,28 @@
-(function(){
-  var app = angular.module('phonecatApp', []);
 
+angular.module('phonecatApp', [])
+.controller('EmailCtrl', ['$scope' '$http', function($scope, $http){
+	$scope.isPopupVisible = false;
 
-  app.controller('EmailCtrl', ['$scope' '$http', function($scope, $http){
-		$scope.isPopupVisible = false;
+  $scope.showPopup = function(email) {
+      $scope.isPopupVisible = true;
+      $scope.selectedEmail = email;
+  };
 
-    $scope.showPopup = function(email) {
-        $scope.isPopupVisible = true;
-        $scope.selectedEmail = email;
-    };
+  $scope.closePopup = function() {
+      $scope.isPopupVisible = false;
+  };
 
-    $scope.closePopup = function() {
-        $scope.isPopupVisible = false;
-    };
+  $scope.messages = function(){
+    $http.post('/messages', {store: 'jakes'}).success(function(data, status, headers, config){
+      if(status == 'ok'){
+        console.log(data);
+        $scope.emails = data.Messages.Message;
+      }else{
+        console.log(data);
+      }
+    }).error(function(data, status, headers, config) {
+      console.log(data.status);
+		});
+  }
 
-    $scope.messages = function(){
-      $http.post('/messages', {store: 'jakes'}).success(function(data, status, headers, config){
-        if(status == 'ok'){
-          console.log(data);
-          $scope.emails = data.Messages.Message;
-        }else{
-          console.log(data);
-        }
-      }).error(function(data, status, headers, config) {
-        console.log(data.status);
-			});
-    }
-    
-	} ]);
-});
+} ]);
