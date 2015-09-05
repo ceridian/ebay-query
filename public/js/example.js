7beb476f4e0556445ffbd891fe61255b34292fd0
Jacob Van Vleet <jacob.vanvleet@gmail.com>
2015-09-05 19:34:56 -0400

    test

--- public/js/example.js
+++ public/js/example.js
@@ -1,50 +1,48 @@
 angular.module('ui.bootstrap.demo', ['ngAnimate', 'ui.bootstrap']);
-angular.module('ui.bootstrap.demo').controller('ModalDemoCtrl', function ($scope, $modal, $log) {
+angular.module('ui.bootstrap.demo').controller('MsgCtrl', function ($scope, $modal, $log) {
 
-  $scope.items = ['item1', 'item2', 'item3'];
+  $scope.emails = [];
 
-  $scope.animationsEnabled = true;
-
-  $scope.open = function (size) {
+  $scope.open = function (email) {
 
     var modalInstance = $modal.open({
-      animation: $scope.animationsEnabled,
       templateUrl: '/temps/modal.html',
       controller: 'ModalInstanceCtrl',
       backdrop: false,
-      size: size,
+      size: 'lg',
       resolve: {
-        items: function () {
-          return $scope.items;
+        email: function () {
+          return email;
         }
       }
     });
 
     modalInstance.result.then(function (selectedItem) {
-      $scope.selected = selectedItem;
+      console.log('done with email');
     }, function () {
       $log.info('Modal dismissed at: ' + new Date());
     });
   };
 
-  $scope.toggleAnimation = function () {
-    $scope.animationsEnabled = !$scope.animationsEnabled;
-  };
-
+  $scope.messages = function(){
+    $http.post('/messages', {store: 'jakes'}).success(function(data, status, headers, config){
+      console.log(data);
+      console.log(data.Messages.Message);
+      $scope.emails = data.Messages.Message;
+    }).error(function(data, status, headers, config) {
+      console.log(data);
+		});
+  }
+  $scope.emails = messages();
 });
 
 // Please note that $modalInstance represents a modal window (instance) dependency.
 // It is not the same as the $modal service used above.
 
-angular.module('ui.bootstrap.demo').controller('ModalInstanceCtrl', function ($scope, $modalInstance, items) {
-
-  $scope.items = items;
-  $scope.selected = {
-    item: $scope.items[0]
-  };
-
+angular.module('ui.bootstrap.demo').controller('ModalInstanceCtrl', function ($scope, $modalInstance, email) {
+  console.log(email);
   $scope.ok = function () {
-    $modalInstance.close($scope.selected.item);
+    $modalInstance.close();
   };
 
   $scope.cancel = function () {
