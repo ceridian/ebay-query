9829b099656eca19b283813802970575e362b7ba
Jacob Van Vleet <jacob.vanvleet@gmail.com>
2015-09-06 14:47:55 -0400

    test

--- routes/router.js
+++ routes/router.js
@@ -7,17 +7,26 @@ module.exports = function(app) {
   });
 
   app.post('/messages', function(req, res){
-    console.log(req.body);
-    lib.messageSummary(req.body.store, function(err, data){
-      if(err){
-        res.send(err);
-      }else{
-        res.send(data);
-      }
-    });
+    var body = req.body;
+    var store = body.store;
+    if(body.length === 0 || store === 'undefined'){
+      res.send('no store sent');
+    }else{
+      lib.messageSummary(store, function(err, data){
+        if(err){
+          res.send(err);
+        }else{
+          res.send(data);
+        }
+      });
+    }
   });
 
   app.post('/msgDetail', function(req, res){
+    var body = req.body;
+    if(body.length == 0){
+      res.send('no store sent');
+    }else{
     lib.messageDetail(req.body.msgID, req.body.store, function(err, data){
       if(err){
         res.send(err);
