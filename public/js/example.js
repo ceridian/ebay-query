deb218bf2106daf1864c092147094e649f7c31a9
Jacob Van Vleet <jacob.vanvleet@gmail.com>
2015-09-07 19:46:23 -0400

    test

--- public/js/example.js
+++ public/js/example.js
@@ -5,12 +5,18 @@
   	var socket = io.connect();
   	return {
   		on: function (eventName, callback) {
-  			socket.on(eventName, function () {
-  				var args = arguments;
-  				$rootScope.$apply(function () {
-  					callback.apply(socket, args);
-  				});
-  			});
+        function wrapper() {
+          var args = arguments;
+          $rootScope.$apply(function () {
+            callback.apply(socket, args);
+          });
+        }
+
+        socket.on(eventName, wrapper);
+        
+        return function () {
+          socket.removeListener(eventName, wrapper);
+        };
   		},
   		emit: function (eventName, data, callback) {
   			socket.emit(eventName, data, function () {
