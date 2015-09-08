01b70238d77e9bd9283857387fe61370f2084b5b
Jacob Van Vleet <jacob.vanvleet@gmail.com>
2015-09-08 13:11:50 -0400

    test

--- public/js/example.js
+++ public/js/example.js
@@ -27,7 +27,7 @@ app.factory('socket', ['$rootScope', function ($rootScope) {
 
 app.controller('MsgCtrl', ['socket', '$modal', '$scope', function(socket, $modal, $scope){
   $scope.emails = [];
-  socket.emit('messagesGet', {store: 'jakes'});
+  socket.emit('messages', {store: 'jakes'});
   socket.on('messages', function(data){
     $scope.emails = data.Messages.Message
   });
@@ -70,91 +70,3 @@ app.controller('ModalInstanceCtrl', function ($scope, $modalInstance, email) {
     $modalInstance.dismiss('cancel');
   };
 });
-/*angular.module('ui.bootstrap.demo', ['ngAnimate', 'ui.bootstrap']);
-
-  angular.module('ui.bootstrap.demo').factory('socket', ['$rootScope', function ($rootScope) {
-  	var socket = io.connect();
-  	return {
-  		on: function (eventName, callback) {
-        function wrapper() {
-          var args = arguments;
-          $rootScope.$apply(function () {
-            callback.apply(socket, args);
-          });
-        }
-
-        socket.on(eventName, wrapper);
-
-        return function () {
-          socket.removeListener(eventName, wrapper);
-        };
-  		},
-  		emit: function (eventName, data, callback) {
-  			socket.emit(eventName, data, function () {
-  				var args = arguments;
-  				$rootScope.$apply(function () {
-  					if (callback) {
-  						callback.apply(socket, args);
-  					}
-  				});
-  			});
-  		}
-  	};
-  }]);
-
-  angular.module('ui.bootstrap.demo').controller('MsgCtrl', ['socket', '$modal', '$scope' function(socket, $modal, $scope){
-    $scope.emails = [];
-    socket.emit('messages', {store: 'jakes'});
-    socket.on('messages', function(data){
-      $scope.emails = data.Messages.Message
-    });
-    socket.on('error', function(err){
-      console.log(err);
-    });
-    socket.on('msgDetail', function(data){
-      console.log(data);
-      $scope.emails = data.Messages.Message;
-      var modalInstance = $modal.open({
-        templateUrl: '/temps/modal.html',
-        controller: 'ModalInstanceCtrl',
-        size: 'lg',
-        resolve: {
-          email: function () {
-            return data;
-          }
-        }
-      });
-      modalInstance.result.then(function (selectedItem) {
-        console.log('done with email');
-      }, function () {
-        $log.info('Modal dismissed at: ' + new Date());
-      });
-    //}).error(function(data, status, headers, config) {
-    });
-    socket.on('msgDetail', function(data){
-      var modalInstance = $modal.open({
-        templateUrl: '/temps/error.html',
-        controller: 'ModalInstanceCtrl',
-        size: 'lg',
-        resolve: {
-          email: function () {
-            return data;
-          }
-        }
-      });
-    });
-    $scope.open = function (email) {
-      //$http.post('/msgDetail', {store: 'jakes', msgID: email.MessageID}).success(function(data, status, headers, config){
-      socket.emit('msgDetail', {store: 'jakes', msgID: email.MessageID});
-    }
-  }]);
-
-  angular.module('ui.bootstrap.demo').controller('ModalInstanceCtrl', function ($scope, $modalInstance, email) {
-    $scope.ok = function () {
-      $modalInstance.close();
-    };
-
-    $scope.cancel = function () {
-      $modalInstance.dismiss('cancel');
-    };
-  });*/
