ba6c26cb7ec813792ae11d6a5cb52709f1e92cae
Jacob Van Vleet <jacob.vanvleet@gmail.com>
2015-09-07 22:26:49 -0400

    test

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var models = require("../models");
+var models = require("./models");
 var http = require('http');
 var lib = require('./lib/lib.js');
 
