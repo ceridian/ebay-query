4816117e5ad4f6e7b59a5d59f4be4fe60e28213c
Jacob Van Vleet <jacob.vanvleet@gmail.com>
2015-09-09 17:14:06 -0400

    test

--- public/js/login.js
+++ public/js/login.js
@@ -1,12 +1,11 @@
 var app = angular.module('login', []);
 
 app.controller('LoginCtrl', ['$http', '$scope', function($http, $scope){
-  $scope.login = function(main){
-    var user = main.user;
-    var pass = main.pass;
+  $scope.login = function(){
+
     var salt = genSalt(4);
     console.log(salt);
-    $http.post('/login', {user: user, pass: pass}).success(function(data, status, headers, config){
+    $http.post('/login', {user: $scope.user, pass: $scope.pass}).success(function(data, status, headers, config){
 
     }).error(function(data, status, headers, config) {
       console.log(data.status);
