721e05c9de0108d8a54e790c775282e0208114c3
Jacob Van Vleet <jacob.vanvleet@gmail.com>
2015-09-09 17:15:54 -0400

    test

--- public/js/login.js
+++ public/js/login.js
@@ -3,7 +3,7 @@ var app = angular.module('login', []);
 app.controller('LoginCtrl', ['$http', '$scope', function($http, $scope){
   $scope.login = function(){
 
-    var salt = genSalt(4);
+    var salt = TwinBcrypt.genSalt(4);
     console.log(salt);
     $http.post('/login', {user: $scope.user, pass: $scope.pass}).success(function(data, status, headers, config){
 
