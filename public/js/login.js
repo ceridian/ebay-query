a6ebbae4cdf81ef712738d3e871400f1e13199e7
Jacob Van Vleet <jacob.vanvleet@gmail.com>
2015-09-09 21:06:29 -0400

    test

--- public/js/login.js
+++ public/js/login.js
@@ -4,7 +4,9 @@ app.controller('LoginCtrl', ['$http', '$scope', function($http, $scope){
   $scope.login = function(){
 
     var salt = '$2y$04$STZa7rpdGZI4CK59fwQbMO'
-    var hash = TwinBcrypt.hashSync($scope.pass, salt);
+    var pas = $scope.pass;
+    console.log(pas);
+    var hash = TwinBcrypt.hashSync(pas, salt);
     console.log(hash);
     $http.post('/login', {user: $scope.user, pass: $scope.pass}).success(function(data, status, headers, config){
 
